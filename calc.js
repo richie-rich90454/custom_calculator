1ace8678081f37e86cea20a0ea87ba41db84e2be
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2024-04-17 20:17:15 +0800

    81717

--- calc.js
+++ calc.js
@@ -6,7 +6,10 @@ let monitor=document.getElementById("monitor");
 let ans=0;
 add.addEventListener("click",function(){
     monitor.innerHTML+="+";
-})
+});
+subtrt.addEventListener("click",function(){
+    monitor.innerHTML+="-";
+});
 del.addEventListener("click",function(){
     if (monitor.innerHTML!=""){
         monitor.innerHTML=monitor.innerHTML.slice(0,-1);
