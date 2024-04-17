0ce04b33c32a73221b8b5b565348a77ba67c38fa
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2024-04-17 20:15:03 +0800

    81417

--- calc.js
+++ calc.js
@@ -1,5 +1,14 @@
 let equal=document.getElementById("equal");
+let del=document.getElementById("del");
+let add=document.getElementById("add");
+let subtrt=document.getElementById("subt");
+let monitor=document.getElementById("monitor");
 let ans=0;
-equal.addEventListener("click",function(){
-    document.getElementById("monitor").innerHTML+="=";
+add.addEventListener("click",function(){
+    monitor.innerHTML+="+";
+})
+del.addEventListener("click",function(){
+    if (monitor.innerHTML!=""){
+        monitor.innerHTML=monitor.innerHTML.slice(0,-1);
+    }
 });
